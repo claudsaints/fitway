d8626ecebf784400e4490459724406e56a504dfb
claudsaints <claudiodsantoss7@gmail.com>
2025-07-04 15:36:41 -0300

    feat: form-ReactookForm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,14 @@ import {NavigationContainer, } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
 import { MyThemeLight } from "theme";
 import { StatusBar } from "expo-status-bar";
+import { AuthRoutes } from "./auth.routes";
 
 
 export function Routes() {
   return (
     <NavigationContainer theme={MyThemeLight as ReactNavigation.Theme}>
       <StatusBar style="light" />
-      <AppRoutes />
+      <AuthRoutes />
     </NavigationContainer>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,6 @@
 import Container from "@components/Container/Container";
 import { styles } from "@styles/index";
-import { View, Image, useWindowDimensions } from "react-native";
+import { View, Text, useWindowDimensions } from "react-native";
 import LogoSvg from "@assets/logo.svg";
 
 import React from "react";
@@ -9,54 +9,167 @@ import { AuthNavigationRoutes } from "@routes/auth.routes";
 import Input from "@components/Input";
 import Button from "@components/Button/Button";
 import { useNavigation } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
+import yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
 
 export default function SignUp() {
+  const validateSchema = new yup.ObjectSchema({
+    name: yup.string().required("Informe o nome"),
+    email: yup.string().required("Informe o email").email("E-mail inválido"),
+    password: yup.string().required("Informe a senha").min(6,"A senha deve ter té 6 digitos"),
+    password_confirm: yup.string().required("Informe a confirmação de senha").oneOf([yup.ref('password')],"As senhas devem ser iguais!"),
+  });
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      password_confirm: "",
+    },
+    resolver: yupResolver(validateSchema),
+  });
+
   const { width, height } = useWindowDimensions();
   const isLandscape = width > height;
-
   const logoSize = Math.max(400, Math.min(width * 0.5, 220));
 
-  const navigaiton = useNavigation<AuthNavigationRoutes>();
+  const navigation = useNavigation<AuthNavigationRoutes>();
+
   function handleBackToLogin() {
-    navigaiton.navigate("signIn");
+    navigation.navigate("signIn");
   }
+
+  const handlerSignUp = (data: FormData) => {
+    console.log("Form Data:", data);
+    console.log("Get Values:", getValues());
+  };
+
+  const handlerError = (error: any) => console.log("Form Errors:", error);
+
   return (
     <Container>
-      <View style={[styles.container, {flexDirection: isLandscape ? 'row' : 'column', alignItems: 'center', justifyContent: 'center'}]}>
-        <LogoSvg 
-          style={{ marginTop: 40 }} width={logoSize} height={logoSize} 
-        />
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: isLandscape ? "row" : "column",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        ]}
+      >
+        <LogoSvg style={{ marginTop: 40 }} width={logoSize} height={logoSize} />
       </View>
 
-      <View style={[styles.vstack, {width: isLandscape ? '50%' : '100%', alignSelf: 'center'}]}>
+      <View
+        style={[
+          styles.vstack,
+          { width: isLandscape ? "50%" : "100%", alignSelf: "center" },
+        ]}
+      >
         <Title variant="h2" style={{ marginBottom: 20 }}>
-          Crie sua conta 
+          Crie sua conta
         </Title>
-         <Input
-          placeholder="Nome"
-          keyboardType="default"
-          autoCapitalize="none"
+
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Nome"
+              keyboardType="default"
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
+
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
-        <Input
-          placeholder="Senha"
-          keyboardType="visible-password"
-          secureTextEntry={true}
-          autoCapitalize="none"
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+              returnKeyType="send"
+              onSubmitEditing={handleSubmit(handlerSignUp)}
+            />
+          )}
         />
-        <Button style={{ marginTop: 16}} textStyle={{ color: "#fff"}}>Cadastrar</Button>
+
+        <Controller
+          control={control}
+          name="password_confirm"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Confirmar Senha"
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+              returnKeyType="send"
+              onSubmitEditing={handleSubmit(handlerSignUp)}
+            />
+          )}
+        />
+
+        <Button
+          onPress={handleSubmit(handlerSignUp, handlerError)}
+          style={{ marginTop: 16 }}
+          textStyle={{ color: "#fff" }}
+        >
+          Cadastrar
+        </Button>
       </View>
-      <View style={[styles.vstack, {width: isLandscape ? '50%' : '100%', alignSelf: 'center'}]}>
-     
-        <Button 
-          style={{ marginTop: 16, backgroundColor: "transparent", borderWidth: 2, borderColor: "black"}} textStyle={{ color: "black", fontWeight: "bold" }}
+
+      <View
+        style={[
+          styles.vstack,
+          { width: isLandscape ? "50%" : "100%", alignSelf: "center" },
+        ]}
+      >
+        <Button
+          style={{
+            marginTop: 16,
+            backgroundColor: "transparent",
+            borderWidth: 2,
+            borderColor: "black",
+          }}
+          textStyle={{ color: "black", fontWeight: "bold" }}
           onPress={handleBackToLogin}
         >
-            Voltar para o login
+          Voltar para o login
         </Button>
       </View>
     </Container>
